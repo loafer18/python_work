121d51ee5dd1ebe2f1cf31a2596bd03f9c07eeec
Yujian Liu <31929880+loafer18@users.noreply.github.com>
2017-09-18 00:39:59 +0800

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+first_name = ("Ethan")
+last_name = ("Liu")
+full_name = first_name + " " + last_name
+
+print ("Hello "+ full_name + ", would you like to learn some Python today?")
+
+print (full_name.title())
+print (full_name.upper())
+print (full_name.lower())
